72cb497f6e4827f6ce40f0e68100c457197c053f
unknown <C:\Users\user\AppData\Roaming\The Bat!>
2019-01-17 16:47:56 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,51 @@
+var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+var obj = {};
+    arr = [];
+    arr2 = [];
+    arr3 = [];
+var values = "";
+var quantity =[];
+
+var num = prompt ('number  of random values');
+var min = prompt ('Pls specify range characters. min characters:');
+var max = prompt ('max characters:');
+num = parseInt(num);
+min = parseInt(min);
+max = parseInt(max);
+
+for (var c = 0; c < num; c ++){
+
+var rand = min + Math.random() * (max + 1 - min);
+rand = Math.floor(rand);
+quantity.push(rand);
+
+            for (var i = 0; i < rand; i++)
+            values += possible.charAt(Math.floor(Math.random() * possible.length));
+
+arr.push (values);
+var values = "";
+}
+
+//считаем значения по символам
+for(i in quantity){
+	if (arr2[quantity[i]]!=undefined) {
+		(arr2[quantity[i]]++)
+	}
+	else {
+		(arr2[quantity[i]]=1)
+	}
+}
+
+//сообщаем пользователю о результатах
+for (var i = min; i < arr2.length; i++ )
+console.log(arr2[i] + ' words ' + i + ' characters');
+
+// Группируем
+for (var c =1 ;min<=max; min++, c++){
+    for (i = 0 ; i < arr.length; i++){
+    if (arr[i].length === min)
+    arr3.push(arr[i]);}
+obj[c] = arr3;
+var arr3=[];}
+
+console.log(obj);
