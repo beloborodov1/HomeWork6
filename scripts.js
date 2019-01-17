858c020a63ab60a8b9768f5e3640f1cbdd3532f7
unknown <C:\Users\user\AppData\Roaming\The Bat!>
2019-01-17 19:24:11 +0200

    FINISH

--- scripts.js
+++ scripts.js
@@ -1,38 +1,31 @@
 var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-var obj = {};
+    obj = {};
     arr = [];
     arr2 = [];
-    arr3 = [];
-var values = "";
-var quantity =[];
+    values = "";
+    num = parseInt(prompt ('number  of random values'));
+    min = parseInt(prompt ('Pls specify range characters. min characters:'));
+    max = parseInt(prompt ('max characters:'));
 
-var num = prompt ('number  of random values');
-var min = prompt ('Pls specify range characters. min characters:');
-var max = prompt ('max characters:');
-num = parseInt(num);
-min = parseInt(min);
-max = parseInt(max);
-
+// получаем массив со всеми сгенрированными значениями
 for (var c = 0; c < num; c ++){
-
 var rand = min + Math.random() * (max + 1 - min);
 rand = Math.floor(rand);
-quantity.push(rand);
 
-            for (var i = 0; i < rand; i++)
-            values += possible.charAt(Math.floor(Math.random() * possible.length));
+        for (var i = 0; i < rand; i++)
+        values += possible.charAt(Math.floor(Math.random() * possible.length));
 
 arr.push (values);
-var values = "";
-}
-// Группируем
+var values = ""}
+
+// разбиваем, группируем, сообщаем
 for (var c =1 ;min<=max; min++, c++){
     for (i = 0 ; i < arr.length; i++){
-    if (arr[i].length === min)
-    arr3.push(arr[i]);
+        if (arr[i].length === min)
+            arr2.push(arr[i]);
     }
-console.log(arr3.length + ' words ' + min + ' characters');
-obj[c] = arr3;
-var arr3=[];}
+console.log(arr2.length + ' words ' + min + ' characters');
+obj[c] = arr2;
+var arr2=[]}
 
 console.log(obj);
